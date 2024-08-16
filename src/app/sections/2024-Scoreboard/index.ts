d8a117d7b18a61b8cf6ca07a0d6f188d1223b715
Thiago Hirata <hirata@devcase.com.br>
2024-08-16 01:30:07 -0300

    thumbnail for scoreboard

--- src/app/sections/2024-Scoreboard/index.ts
+++ src/app/sections/2024-Scoreboard/index.ts
@@ -4,7 +4,7 @@ const section: Section = {
   title: "Scoreboard",
   subtitle: "A simple scoreboard for playing board games.",
   badge: "2024",
-  imgSrc: require("./screenshot-01.png"),
+  imgSrc: require("./screenshot-01.jpg"),
   imgAlt: "Scoreboard",
   href: "https://scoreboard.hirata.dev.br",
   sourceUrl: "https://github.com/thiagohirata/scoreboard",
